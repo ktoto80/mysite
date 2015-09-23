40a24cc0b17f1540ffa357b37203e7ca492f6675
Dew25 <pupil@debian64>
2015-09-23 16:34:52 +0300

    первый коммит

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Список всех записей в таблице</h1>
+		<ol>
+			<li>
+				<?php echo "сюда поместим записи"; ?>
+			</li>
+		</ol>
+</body>
+</html>
